2cd23404f228ee833b6f0d2e8ddf8c0d0ea85400
= <=>
2023-06-28 23:57:58 +0100

    Added flatpak to apt install list

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade -y
 echo "Update done." >> setup.log
 
 # apt
-sudo apt install git nodejs npm nodejs python3 p7zip-full p7zip-rar -y
+sudo apt install git flatpak nodejs npm nodejs python3 p7zip-full p7zip-rar -y
 
 echo "Done with apt installations." >> setup.log
 
