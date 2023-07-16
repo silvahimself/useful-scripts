e8252e42a77953a1e1956370184777dda95747c3
= <=>
2023-07-16 13:55:29 +0100

    added dislocker to apt list

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 echo "running setup script..." > setup.log
 
+export install_list='git virtualbox virtualbox-guest-additions-iso virtualbox-guest-utils flatpak nodejs neofetch npm nodejs python3 python3-dev python3-pip python3-setuptools p7zip-full p7zip-rar dislocker'
+
 sudo apt update && sudo apt upgrade -y
 echo "Update done." >> setup.log
 
 # apt
-sudo apt install git virtualbox virtualbox-guest-additions-iso virtualbox-guest-utils flatpak nodejs neofetch npm nodejs python3 p7zip-full p7zip-rar -y
+sudo apt install $install_list -y
 
-echo "Done with apt installations." >> setup.log
+# pip
+pip3 install thefuck --user
+
+echo 'Done with apt installations.\nInstalled: $install_list' >> setup.log
 
 # Snaps
 sudo snap install rider --classic
